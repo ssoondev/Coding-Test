d4963fb75267dc3645b269ae61dc5093b4f66ab6
ssoondev <ssoonearth@gmail.com>
2025-06-05 15:25:11 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.06 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder(my_string);
+
+        sb.delete(s, s + overwrite_string.length());
+        sb.insert(s, overwrite_string);
+
+        return sb.toString();
+    }
+}
