34e824e045b7ec7b1a089214663e4fa8c1c5e9d9
ssoondev <ssoonearth@gmail.com>
2025-06-05 15:27:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -4,8 +4,7 @@ class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         StringBuilder sb = new StringBuilder(my_string);
 
-        sb.delete(s, s + overwrite_string.length());
-        sb.insert(s, overwrite_string);
+        sb.replace(s, s + overwrite_string.length(), overwrite_string);
 
         return sb.toString();
     }
